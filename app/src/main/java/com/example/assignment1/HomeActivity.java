b3be9a4fd9cb1b8355f73f9a2e67538cd4c73ece
Izen Abbas <l230748@lhr.nu.edu.pk>
2026-03-27 13:59:06 +0500

    Initial Commit

---
+++ app/src/main/java/com/example/assignment1/HomeActivity.java
@@ -0,0 +1,32 @@
+package com.example.assignment1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        AppCompatButton btnBookMovie1 = findViewById(R.id.btnBookMovie1);
+        AppCompatButton btnBookMovie2 = findViewById(R.id.btnBookMovie2);
+        AppCompatButton btnBookMovie3 = findViewById(R.id.btnBookMovie3);
+        AppCompatButton btnBookMovie4 = findViewById(R.id.btnBookMovie4);
+
+        View.OnClickListener bookListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, SeatSelectionActivity.class));
+            }
+        };
+
+        btnBookMovie1.setOnClickListener(bookListener);
+        btnBookMovie2.setOnClickListener(bookListener);
+        btnBookMovie3.setOnClickListener(bookListener);
+        btnBookMovie4.setOnClickListener(bookListener);
+    }
+}
---
+++ app/src/main/java/com/example/assignment1/OnboardingActivity.java
@@ -0,0 +1,24 @@
+package com.example.assignment1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OnboardingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+
+        Button btnGetStarted = findViewById(R.id.btnGetStarted);
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(OnboardingActivity.this, HomeActivity.class));
+                finish();
+            }
+        });
+    }
+}
