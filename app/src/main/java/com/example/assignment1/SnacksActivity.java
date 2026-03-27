2a1ee5787265573e89682134385ef842d5c5dd8d
Izen Abbas <l230748@lhr.nu.edu.pk>
2026-03-27 14:11:57 +0500

    Architecture shift and data models.

---
+++ app/src/main/java/com/example/assignment1/SnacksActivity.java
@@ -0,0 +1,23 @@
+package com.example.assignment1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SnacksActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snacks);
+
+        Button btnConfirmOrder = findViewById(R.id.btnConfirmOrder);
+        btnConfirmOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SnacksActivity.this, TicketSummaryActivity.class));
+            }
+        });
+    }
+}
